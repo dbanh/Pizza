7962a7734ba816518e77718d2180493513d12ad1
Denise <dbanh@pdx.edu>
2016-07-21 18:48:27 -0700

    Added new pizza icons

---
+++ Pizza/CustomerOrder.swift
@@ -0,0 +1,25 @@
+//
+//  CustomerOrder.swift
+//  Pizza
+//
+//  Created by Denise Banh on 7/21/16.
+//  Copyright © 2016 Denise Banh. All rights reserved.
+//
+
+import UIKit
+
+/*
+class CustomerOrder {
+    // MARK: Properties
+    
+    var size: String
+    var tomato: Bool
+    var cheese: Bool
+    var peppers: Bool
+    var onions: Bool
+    var sausage: Bool
+    var ham: Bool
+    var chicken: Bool
+    
+}
+ */
