ffa5796f84674dc7703456a891586520d0b5fa23
Denise <dbanh@pdx.edu>
2016-07-21 18:29:09 -0700

    Added UIStepper functionality

---
+++ Pizza/SecondViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SecondViewController.swift
+//  Pizza
+//
+//  Created by Denise Banh on 7/21/16.
+//  Copyright © 2016 Denise Banh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var Small: UILabel!
+
+    @IBAction func SmallStepper(sender: UIStepper) {
+        self.Small.text = sender.value.description
+    }
+    
+    
+    @IBOutlet weak var Medium: UILabel!
+    
+    @IBAction func MediumStepper(sender: UIStepper) {
+        self.Medium.text = sender.value.description
+    }
+    
+    @IBOutlet weak var Large: UILabel!
+    
+    @IBAction func LargeStepper(sender: UIStepper) {
+        self.Large.text = sender.value.description
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
